8f411264deb9900cd5c0ab2bd626d8a76f437496
w5678rr <163996857+w5678rr@users.noreply.github.com>
2024-05-18 16:10:28 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -34,12 +34,12 @@ export function score(rank, percent, minPercent, levelCount) {
 
 export function calculateScores(levelCount) {
     const b = (levelCount - 1) * baseFactor;
-    const a = 6000 * Math.sqrt(b);
+    const a = 600 * Math.sqrt(b);
 
     let scores = [];
     for (let rank = 0; rank < levelCount; ++rank) {
         const score = (a / Math.sqrt(rank / 50 + b) - 100);
-        scores.push(round(score));
+        scores.push(score);
     }
 
     return scores;
