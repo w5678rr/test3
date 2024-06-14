20c42915ea4a41aa010d487b799c33579b221432
w5678rr <163996857+w5678rr@users.noreply.github.com>
2024-06-14 13:58:20 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -44,5 +44,20 @@ export function calculateScores(levelCount) {
 
     return scores;
 }
-
+export function round(num) {
+    if (!('' + num).includes('e')) {
+        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
+    } else {
+        var arr = ('' + num).split('e');
+        var sig = '';
+        if (+arr[1] + scale > 0) {
+            sig = '+';
+        }
+        return +(
+            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
+            'e-' +
+            scale
+        );
+    }
+}
 
